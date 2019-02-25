918a1b82e0f3118a4c185787e1251c05e95b3fcc
kimharry99 <kimharry99@korea.ac.kr>
2019-02-26 06:04:54 +0900

    임시 메인 함수

--- src/summonersWarRuneCalculator/Main.java
+++ src/summonersWarRuneCalculator/Main.java
@@ -1,10 +1,17 @@
 package summonersWarRuneCalculator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		Rune rune = new RuneManager().scanRune();
+		double temp = new RuneCalculation().calculateRuneScore(rune);
+		
+		scanner.close();
 	}
 
 }

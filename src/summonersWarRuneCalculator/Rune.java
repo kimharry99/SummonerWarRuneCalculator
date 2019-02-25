17701dc55e158088bf3182f8c359f2378bfb3432
kimharry99 <kimharry99@korea.ac.kr>
2019-02-26 05:59:02 +0900

    룬 타입 문자열 배열 추가

--- src/summonersWarRuneCalculator/Rune.java
+++ src/summonersWarRuneCalculator/Rune.java
@@ -1,66 +1,67 @@
 package summonersWarRuneCalculator;
 
 public class Rune {
-	int type, number;
+	String[] typeArray = {"폭주","신속","반격","칼날","집중","활력","맹공","절망","수호","인내","보호"};
+	int type=0, number=1;
 	int rarity = 4;
 	int runeLevel=12;
 	int []mainStat = { 0, 0, 0, 0, 0, 0, 0, 0 };
 	int[] subStatArray = { 0, 0, 0, 0, 0, 0, 0, 0 };
 	int []prefixStat = { 0, 0, 0, 0, 0, 0, 0, 0 };
-	String monster;
-	boolean searchable;
+	String monster = "물피닉스";
+	boolean searchable=true;
 
-	public Rune(int _type, int _number, int[] _mainStat, int[] _statArray) {
+	public Rune(int _type, int _number, int[] _mainStat, int[] _subStatArray) {
 		this.type = _type;
 		this.number = _number;
 		this.mainStat = _mainStat;
-		this.subStatArray = _statArray;
+		this.subStatArray = _subStatArray;
 	}
 
-	public Rune(int _type, int _number, int[] _mainStat, int[] _prefixStat, int[] _statArray) {
+	public Rune(int _type, int _number, int[] _mainStat, int[] _prefixStat, int[] _subStatArray) {
 		this.type = _type;
 		this.number = _number;
 		this.mainStat = _mainStat;
 		this.prefixStat = _prefixStat;
-		this.subStatArray = _statArray;
+		this.subStatArray = _subStatArray;
 	}
 	
-	public Rune(int _type, int _number, int _rarity, int[] _mainStat, int[] _prefixStat, int[] _statArray) {
+	public Rune(int _type, int _number, int _rarity, int[] _mainStat, int[] _prefixStat, int[] _subStatArray) {
 		this.type = _type;
 		this.number = _number;
 		this.rarity=_rarity;
 		this.mainStat = _mainStat;
 		this.prefixStat = _prefixStat;
-		this.subStatArray = _statArray;
+		this.subStatArray = _subStatArray;
 	}
 
-	public Rune(int _type, int _number, int _rarity, int _runeLevel, int[] _mainStat, int[] _prefixStat, int[] _statArray) {
+	public Rune(int _type, int _number, int _rarity, int _runeLevel, int[] _mainStat, int[] _prefixStat, int[] _subStatArray) {
 		this.type = _type;
 		this.number = _number;
 		this.rarity=_rarity;
 		this.runeLevel=_runeLevel;
 		this.mainStat = _mainStat;
 		this.prefixStat = _prefixStat;
-		this.subStatArray = _statArray;
+		this.subStatArray = _subStatArray;
 	}
 
 	
-	public Rune(int _type, int _number, int[] _mainStat, int[] _prefixStat, int[] _statArray, String _monster) {
+	public Rune(int _type, int _number, int[] _mainStat, int[] _prefixStat, int[] _subStatArray, String _monster) {
 		this.type = _type;
 		this.number = _number;
 		this.mainStat = _mainStat;
 		this.prefixStat = _prefixStat;
-		this.subStatArray = _statArray;
+		this.subStatArray = _subStatArray;
 		this.monster = _monster;
 	}
 
-	public Rune(int _type, int _number, int[] _mainStat, int[] _prefixStat, int[] _statArray, String _monster,
+	public Rune(int _type, int _number, int[] _mainStat, int[] _prefixStat, int[] _subStatArray, String _monster,
 			boolean _searchable) {
 		this.type = _type;
 		this.number = _number;
 		this.mainStat = _mainStat;
 		this.prefixStat = _prefixStat;
-		this.subStatArray = _statArray;
+		this.subStatArray = _subStatArray;
 		this.monster = _monster;
 		this.searchable = _searchable;
 	}

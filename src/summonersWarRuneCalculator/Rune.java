b7f028355794a5c2ce73918b0de5e0ca1cdbe073
kimharry99 <kimharry99@korea.ac.kr>
2019-02-26 11:30:44 +0900

    스텟 등급을 룬 내부의 변수로 추가 + get/set 메소드 추가

--- src/summonersWarRuneCalculator/Rune.java
+++ src/summonersWarRuneCalculator/Rune.java
@@ -7,23 +7,99 @@ public class Rune {
 	int runeLevel=12;
 	int []mainStat = { 0, 0, 0, 0, 0, 0, 0, 0 };
 	int[] subStatArray = { 0, 0, 0, 0, 0, 0, 0, 0 };
+	double[][] statGradeArray=new double [2][8];
 	int []prefixStat = { 0, 0, 0, 0, 0, 0, 0, 0 };
 	String monster = "pheonix (water)";
 	boolean searchable=true;
 
+	public int getRarity() {
+		return this.rarity;
+	}
+	public void setRarity(int _rarity) {
+		this.rarity=_rarity;
+	}
+	public int getRuneLevel() {
+		return this.runeLevel;
+	}
+	public void setruneLevel(int _runeLevel) {
+		this.runeLevel=_runeLevel;
+	}
+	public int[] getSubStatArray() {
+		return subStatArray;
+	}
+	public void setSubStatArray(int [] _subStatArray) {
+		this.subStatArray=_subStatArray;
+		for (int i = 0; i < statGradeArray[1].length; i++) {
+			if (0 <= i && i <= 2) {
+				statGradeArray[1][i] = _subStatArray[i] / 6.5;
+			} else if (2 < i && i < 5) {
+				statGradeArray[1][i] = _subStatArray[i] / 5.0;
+			} else if (i == 5) {
+				statGradeArray[1][i] = _subStatArray[i] / 5.5;
+			} else {
+				statGradeArray[1][i] = _subStatArray[i] / 6.0;
+			}
+		}
+	}
+	public int[] getprefixStat() {
+		return prefixStat;
+	}
+	public void setPrefixStat(int [] _prefixStat) {
+		this.prefixStat=_prefixStat;
+		for (int i = 0; i < statGradeArray[1].length; i++) {
+			if (0 <= i && i <= 2) {
+				statGradeArray[0][i] = _prefixStat[i] / 6.5;
+			} else if (2 < i && i < 5) {
+				statGradeArray[0][i] = _prefixStat[i] / 5.0;
+			} else if (i == 5) {
+				statGradeArray[0][i] = _prefixStat[i] / 5.5;
+			} else {
+				statGradeArray[0][i] = _prefixStat[i] / 6.0;
+			}
+		}
+	}
+	public double[][] getStatGradeArray() {
+		return statGradeArray;
+	}
+	public void setStatGradeArray(double[][] _statGradeArray) {
+		this.statGradeArray = _statGradeArray;
+		for(int i=0;i<statGradeArray[0].length;i++) {
+			if (0 <= i && i <= 2) {
+				prefixStat[i]=(int)Math.round(statGradeArray[0][i]*6.5);
+			} else if (2 < i && i < 5) {
+				prefixStat[i]=(int)Math.round(statGradeArray[0][i]*5);
+			} else if (i == 5) {
+				prefixStat[i]=(int)Math.round(statGradeArray[0][i]*5.5);
+			} else {
+				prefixStat[i]=(int)Math.round(statGradeArray[0][i]*6);
+			}
+		}
+		for(int i=0;i<statGradeArray[1].length;i++) {
+			if (0 <= i && i <= 2) {
+				subStatArray[i]=(int)Math.round(statGradeArray[1][i]*6.5);
+			} else if (2 < i && i < 5) {
+				subStatArray[i]=(int)Math.round(statGradeArray[1][i]*5);
+			} else if (i == 5) {
+				subStatArray[i]=(int)Math.round(statGradeArray[1][i]*5.5);
+			} else {
+				subStatArray[i]=(int)Math.round(statGradeArray[1][i]*6);
+			}
+		}
+	}
+	
 	public Rune(int _type, int _number, int[] _mainStat, int[] _subStatArray) {
 		this.type = _type;
 		this.number = _number;
 		this.mainStat = _mainStat;
-		this.subStatArray = _subStatArray;
+		this.setSubStatArray(_subStatArray);
 	}
 
 	public Rune(int _type, int _number, int[] _mainStat, int[] _prefixStat, int[] _subStatArray) {
 		this.type = _type;
 		this.number = _number;
 		this.mainStat = _mainStat;
-		this.prefixStat = _prefixStat;
-		this.subStatArray = _subStatArray;
+		this.setPrefixStat(_prefixStat);
+		this.setSubStatArray(_subStatArray);
 	}
 	
 	public Rune(int _type, int _number, int _rarity, int[] _mainStat, int[] _prefixStat, int[] _subStatArray) {
@@ -31,8 +107,8 @@ public class Rune {
 		this.number = _number;
 		this.rarity=_rarity;
 		this.mainStat = _mainStat;
-		this.prefixStat = _prefixStat;
-		this.subStatArray = _subStatArray;
+		this.setPrefixStat(_prefixStat);
+		this.setSubStatArray(_subStatArray);
 	}
 
 	public Rune(int _type, int _number, int _rarity, int _runeLevel, int[] _mainStat, int[] _prefixStat, int[] _subStatArray) {
@@ -41,8 +117,8 @@ public class Rune {
 		this.rarity=_rarity;
 		this.runeLevel=_runeLevel;
 		this.mainStat = _mainStat;
-		this.prefixStat = _prefixStat;
-		this.subStatArray = _subStatArray;
+		this.setPrefixStat(_prefixStat);
+		this.setSubStatArray(_subStatArray);
 	}
 
 	
@@ -50,8 +126,8 @@ public class Rune {
 		this.type = _type;
 		this.number = _number;
 		this.mainStat = _mainStat;
-		this.prefixStat = _prefixStat;
-		this.subStatArray = _subStatArray;
+		this.setPrefixStat(_prefixStat);
+		this.setSubStatArray(_subStatArray);
 		this.monster = _monster;
 	}
 
@@ -60,8 +136,8 @@ public class Rune {
 		this.type = _type;
 		this.number = _number;
 		this.mainStat = _mainStat;
-		this.prefixStat = _prefixStat;
-		this.subStatArray = _subStatArray;
+		this.setPrefixStat(_prefixStat);
+		this.setSubStatArray(_subStatArray);
 		this.monster = _monster;
 		this.searchable = _searchable;
 	}
--- src/summonersWarRuneCalculator/RuneCalculation.java
+++ src/summonersWarRuneCalculator/RuneCalculation.java
@@ -4,104 +4,78 @@ public class RuneCalculation {
 	double[][] scoreArray = { { 0.5, 1, 0.5, 1, 1, 1, 0, 0 }, { 1, 1, 0.5, 0.5, 1, 1, 0, 0 },
 			{ 0.5, 0, 1, 1, 1, 1, 0.5, 0 }, { 1, 0, 1, 1, 0, 0, 1, 0.5 }, { 1, 0, 1, 1, 0, 0, 1, 0.5 },
 			{ 1, 1, 0.5, 1, 1, 0.5, 0, 0 }, { 1, 0.5, 1, 1, 0, 0, 0.5, 1 }, { 1, 0, 1, 1, 1, 0.5, 0, 0.5 } };
-	String[] roleArray= {"Main Dealer","Raid Dealer","DEF Dealer","Takner","Healer","ATK Healer","Debuffer","Bruiser"};
+	String[] roleArray = { "Main Dealer", "Raid Dealer", "DEF Dealer", "Takner", "Healer", "ATK Healer", "Debuffer",
+			"Bruiser" };
 
 	public double calculateRuneScore(Rune _rune) {
-		double[][] statGradeArray = this.calculateStatGrade(_rune.prefixStat, _rune.subStatArray);
-		int maxScoreRole=0;
+		double[][] statGradeArray = _rune.statGradeArray;
+		int maxScoreRole = 0;
 		double runeScore = 0;
 		for (int i = 0; i < scoreArray.length; i++) {
-			double tempScore =0;
-			if(this.checkRuneMainStat(_rune.mainStat)[i]) {
+			double tempScore = 0;
+			if (this.checkRuneMainStat(_rune.mainStat)[i]) {
 				for (int j = 0; j < scoreArray[i].length; j++) {
 					tempScore += statGradeArray[0][j] * scoreArray[i][j];
 					tempScore += statGradeArray[1][j] * scoreArray[i][j];
 				}
 			}
-			if(runeScore<tempScore) {
+			if (runeScore < tempScore) {
 				runeScore = tempScore;
-				maxScoreRole=i;
+				maxScoreRole = i;
 			}
 		}
-		System.out.println("The best role of this rune is "+roleArray[maxScoreRole]+" and the score is "+runeScore);
+		System.out
+				.println("The best role of this rune is " + roleArray[maxScoreRole] + " and the score is " + runeScore);
 		return runeScore;
 	}
 
 	public void calculateRunePotentional(Rune _rune) {
-		double[][] statGradeArray = this.calculateStatGrade(_rune.prefixStat, _rune.subStatArray);
+		double[][] statGradeArray = _rune.getStatGradeArray();
 		
 	}
 
-	public double[][] calculateStatGrade(int[] _prefixStat, int[] _subStat) {
-		double[][] gradeArray = new double[2][8];
-		for (int i = 0; i < gradeArray[0].length; i++) {
-			if (0 <= i && i <= 2) {
-				gradeArray[0][i] = _prefixStat[i] / 6.5;
-			} else if (2 < i && i < 5) {
-				gradeArray[0][i] = _prefixStat[i] / 5.0;
-			} else if (i == 5) {
-				gradeArray[0][i] = _prefixStat[i] / 5.5;
-			} else {
-				gradeArray[0][i] = _prefixStat[i] / 6.0;
-			}
-		}
-		for (int i = 0; i < gradeArray[1].length; i++) {
-			if (0 <= i && i <= 2) {
-				gradeArray[1][i] = _subStat[i] / 6.5;
-			} else if (2 < i && i < 5) {
-				gradeArray[1][i] = _subStat[i] / 5.0;
-			} else if (i == 5) {
-				gradeArray[1][i] = _subStat[i] / 5.5;
-			} else {
-				gradeArray[1][i] = _subStat[i] / 6.0;
-			}
-		}
-		System.out.println("statGradeArray : "+gradeArray[1][3]);
-		return gradeArray;
-	}
-
 	public boolean[] checkRuneMainStat(int[] _mainStat) {
-		boolean[] booleanArray = {true,true,true,true,true,true,true,true,};
+		boolean[] booleanArray = { true, true, true, true, true, true, true, true, };
 		for (int i = 0; i < _mainStat.length; i++) {
 			if (_mainStat[i] != 0) {
 				switch (i) {
 				case 0:
-					booleanArray[0]=false;
-					booleanArray[2]=false;
+					booleanArray[0] = false;
+					booleanArray[2] = false;
 					break;
 				case 1:
-					booleanArray[2]=false;
-					booleanArray[3]=false;
-					booleanArray[5]=false;
-					booleanArray[7]=false;
+					booleanArray[2] = false;
+					booleanArray[3] = false;
+					booleanArray[5] = false;
+					booleanArray[7] = false;
 					break;
 				case 2:
-					booleanArray[0]=false;
+					booleanArray[0] = false;
 					break;
 				case 3:
-					booleanArray[3]=false;
+					booleanArray[3] = false;
 					break;
 				case 4:
-					booleanArray[3]=false;
-					booleanArray[4]=false;
-					booleanArray[6]=false;
+					booleanArray[3] = false;
+					booleanArray[4] = false;
+					booleanArray[6] = false;
 					break;
 				case 5:
-					booleanArray[3]=false;
-					booleanArray[4]=false;
-					booleanArray[6]=false;
+					booleanArray[3] = false;
+					booleanArray[4] = false;
+					booleanArray[6] = false;
 					break;
 				case 6:
-					booleanArray[0]=false;
-					booleanArray[1]=false;
-					booleanArray[5]=false;
-					booleanArray[7]=false;
+					booleanArray[0] = false;
+					booleanArray[1] = false;
+					booleanArray[5] = false;
+					booleanArray[7] = false;
 					break;
 				case 7:
-					booleanArray[0]=false;
-					booleanArray[1]=false;
-					booleanArray[2]=false;
-					booleanArray[5]=false;
+					booleanArray[0] = false;
+					booleanArray[1] = false;
+					booleanArray[2] = false;
+					booleanArray[5] = false;
 					break;
 				default:
 					break;
